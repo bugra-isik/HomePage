8a9ed80f7bc7b92688f2285ff26c44ca0f0fbdcd
Buğra <librouse98@gmail.com>
2023-08-18 17:28:44 +0300

    v5.2

--- components/main/Home/Home.tsx
+++ components/main/Home/Home.tsx
@@ -1,30 +1,36 @@
 import MainContext from "@/context/Context";
 import { motion } from "framer-motion";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 
 export default function Home() {
   const { arr } = useContext(MainContext);
+  const hiddenity = [true, false];
 
-  useEffect(() => {
-    const videoElement = document.getElementById("video") as HTMLVideoElement;
-    if (videoElement) {
-      videoElement.play();
-    }
-  }, []);
+  let opa = ["opacity-100", "opacity-0"];
 
-  const video: string[] = ["/dark.mp4", "/light.mp4"];
   return (
-    <motion.video
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      key={arr}
-      id="video"
-      className={`select-none rounded`}
-      autoPlay
-      muted
-      loop
-    >
-      <source src={video[arr]} type="video/mp4" />
-    </motion.video>
+    <>
+      <div className={`${opa[arr]} transition duration-300 `}>
+        <motion.img
+          className={`rounded-lg
+        `}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          hidden={!hiddenity[arr]}
+          src="https://onedrive.live.com/embed?resid=C9DCC7798DEEEC1D%2116865&authkey=%21AGDkD01QezbgB9w&width=2560&height=1440"
+          alt="img"
+        />
+      </div>
+      <div className={`${opa[arr == 0 ? 1 : 0]} transition duration-300 `}>
+        <motion.img
+          className={`rounded-lg`}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          hidden={hiddenity[arr]}
+          src="https://onedrive.live.com/embed?resid=C9DCC7798DEEEC1D%2116864&authkey=%21AGKVXAT2CzDVj4U&width=3840&height=2160"
+          alt="img"
+        />
+      </div>
+    </>
   );
 }
--- components/nav/languageSwitch/LanguageSwitch.tsx
+++ components/nav/languageSwitch/LanguageSwitch.tsx
@@ -1,6 +1,6 @@
 import MainContext from "@/context/Context";
 import { motion } from "framer-motion";
-import { useCallback, useContext, useEffect, useRef} from "react";
+import { useCallback, useContext, useEffect, useRef } from "react";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 const LanguageSwitch = () => {
@@ -21,6 +21,7 @@ const LanguageSwitch = () => {
 
   const hoverColor = ["hover:text-yellow", "hover:text-blue"];
   const hidden = ["", "hidden"];
+  let opa = ["opacity-100", "opacity-0"];
 
   return (
     <motion.div
@@ -29,23 +30,27 @@ const LanguageSwitch = () => {
       className="hidden cursor-pointer sm:block 
     "
     >
-      <div
-        className={`${text[arr]} ${hoverColor[arr]} ${hidden[langArr]} transition duration-300`}
-        title={mouseOver}
-        onClick={() => handle()}
-      >
-        {" "}
-        <span className="fi fi-tr"></span>
+      <div className={`${opa[langArr]} transition duration-300 `}>
+        <div
+          className={`${text[arr]} ${hoverColor[arr]} ${hidden[langArr]} transition duration-300`}
+          title={mouseOver}
+          onClick={() => handle()}
+        >
+          {" "}
+          <span className="fi fi-tr"></span>
+        </div>
       </div>
-      <div
-        className={`${text[arr]} ${hoverColor[arr]} ${
-          hidden[langArr == 0 ? 1 : 0]
-        }   transition duration-300`}
-        ref={ref}
-        title={mouseOver}
-        onClick={() => handle()}
-      >
-        <span className="fi fi-us"></span>
+      <div className={`${opa[langArr == 0 ? 1 : 0]} transition duration-300 `}>
+        <div
+          className={`${text[arr]} ${hoverColor[arr]} ${
+            hidden[langArr == 0 ? 1 : 0]
+          }   transition duration-300`}
+          ref={ref}
+          title={mouseOver}
+          onClick={() => handle()}
+        >
+          <span className="fi fi-us"></span>
+        </div>
       </div>
     </motion.div>
   );
