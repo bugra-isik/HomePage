1423bb5a2aa284586bcf392220cd7848043a9518
Buğra <librouse98@gmail.com>
2023-11-03 02:41:30 +0300

    v6.0
    -New Project.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["drive.google.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
